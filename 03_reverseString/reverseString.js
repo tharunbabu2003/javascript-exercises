e32ba43bc443a758875e5b9b37b861147510aa88
Tharun-Babu <tharunbabu27@gmail.com>
2023-08-21 00:52:05 +0530

    Modification of reverseString function
    the function returns reverse of input string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let revString="";
+    const len = string.length;
+for(let i = 0;i<len;i++){
+    revString+=string[len-i-1];
+}
+return revString;
 };
 
 // Do not edit below this line
