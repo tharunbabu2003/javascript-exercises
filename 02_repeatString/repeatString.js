570046b397e6beefe2c77e369fff852a06694229
Tharun-Babu <tharunbabu27@gmail.com>
2023-08-21 00:05:08 +0530

    Modified repeatString function
    the function repeats the input string a given number of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string , num) {
+    output=""
+    while(num--){
+        output+=string
+    }
+    return output
 };
 
 // Do not edit below this line
